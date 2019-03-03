10f344f12fa130b36d779d50489b08c077b5aa27
xdwkeep <xdwkeep@foxmail.com>
2019-03-03 12:13:30 +0800

    arttemplate

---
+++ face_meeting/src/main/webapp/page/test/json.js
@@ -0,0 +1,177 @@
+var data = [{
+    "mNo": 1,
+    "rId": "CR001",
+    "startDate": {
+        "year": 2019,
+        "month": "MARCH",
+        "dayOfYear": 62,
+        "leapYear": false,
+        "dayOfMonth": 3,
+        "dayOfWeek": "SUNDAY",
+        "era": "CE",
+        "monthValue": 3,
+        "chronology": {
+            "id": "ISO",
+            "calendarType": "iso8601"
+        }
+    },
+    "startTime": {
+        "hour": 17,
+        "minute": 8,
+        "second": 8,
+        "nano": 0
+    },
+    "endTime": {
+        "hour": 18,
+        "minute": 7,
+        "second": 11,
+        "nano": 0
+    },
+    "meeting": {
+        "mNo": 1,
+        "mTitle": "关于吃饭的会议",
+        "mInfo": "如何吃饭是大事",
+        "mSize": 20,
+        "mSpan": 45,
+        "tName": "default",
+        "pId_FQ": "user02",
+        "state": null,
+        "mCreateTime": 1551776650000,
+        "creator": null,
+        "mEventList": null,
+        "mAttendList": null,
+        "mInformList": null
+    }
+}, {
+    "mNo": 2,
+    "rId": "CR301",
+    "startDate": {
+        "year": 2019,
+        "month": "MARCH",
+        "dayOfYear": 64,
+        "leapYear": false,
+        "dayOfMonth": 5,
+        "dayOfWeek": "TUESDAY",
+        "era": "CE",
+        "monthValue": 3,
+        "chronology": {
+            "id": "ISO",
+            "calendarType": "iso8601"
+        }
+    },
+    "startTime": {
+        "hour": 17,
+        "minute": 13,
+        "second": 39,
+        "nano": 0
+    },
+    "endTime": {
+        "hour": 18,
+        "minute": 13,
+        "second": 43,
+        "nano": 0
+    },
+    "meeting": {
+        "mNo": 2,
+        "mTitle": "关于吃饭的会议2",
+        "mInfo": "人生大事",
+        "mSize": 20,
+        "mSpan": 45,
+        "tName": "default",
+        "pId_FQ": "user01",
+        "state": null,
+        "mCreateTime": 1551691488000,
+        "creator": null,
+        "mEventList": null,
+        "mAttendList": null,
+        "mInformList": null
+    }
+}, {
+    "mNo": 3,
+    "rId": "CR302",
+    "startDate": {
+        "year": 2019,
+        "month": "MARCH",
+        "dayOfYear": 65,
+        "leapYear": false,
+        "dayOfMonth": 6,
+        "dayOfWeek": "WEDNESDAY",
+        "era": "CE",
+        "monthValue": 3,
+        "chronology": {
+            "id": "ISO",
+            "calendarType": "iso8601"
+        }
+    },
+    "startTime": {
+        "hour": 17,
+        "minute": 14,
+        "second": 11,
+        "nano": 0
+    },
+    "endTime": {
+        "hour": 18,
+        "minute": 14,
+        "second": 13,
+        "nano": 0
+    },
+    "meeting": {
+        "mNo": 3,
+        "mTitle": "t15创建的会议",
+        "mInfo": "没有说明",
+        "mSize": 20,
+        "mSpan": 45,
+        "tName": "default",
+        "pId_FQ": "t15",
+        "state": null,
+        "mCreateTime": 1551618969000,
+        "creator": null,
+        "mEventList": null,
+        "mAttendList": null,
+        "mInformList": null
+    }
+}, {
+    "mNo": 4,
+    "rId": "CR303",
+    "startDate": {
+        "year": 2019,
+        "month": "MARCH",
+        "dayOfYear": 68,
+        "leapYear": false,
+        "dayOfMonth": 9,
+        "dayOfWeek": "SATURDAY",
+        "era": "CE",
+        "monthValue": 3,
+        "chronology": {
+            "id": "ISO",
+            "calendarType": "iso8601"
+        }
+    },
+    "startTime": {
+        "hour": 17,
+        "minute": 14,
+        "second": 35,
+        "nano": 0
+    },
+    "endTime": {
+        "hour": 19,
+        "minute": 14,
+        "second": 37,
+        "nano": 0
+    },
+    "meeting": {
+        "mNo": 4,
+        "mTitle": "user01创建的会议",
+        "mInfo": "没有说明",
+        "mSize": 20,
+        "mSpan": 45,
+        "tName": "default",
+        "pId_FQ": "user02",
+        "state": null,
+        "mCreateTime": 1551705666000,
+        "creator": null,
+        "mEventList": null,
+        "mAttendList": null,
+        "mInformList": null
+    }
+}]
