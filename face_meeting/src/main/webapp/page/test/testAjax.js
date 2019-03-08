ef259d79ccad38b2bcda52ca25435d604e5bbb37
xdwkeep <xdwkeep@foxmail.com>
2019-03-08 13:37:56 +0800

    none

--- face_meeting/src/main/webapp/page/test/testAjax.js
+++ face_meeting/src/main/webapp/page/test/testAjax.js
@@ -8,22 +8,22 @@ layui.use(['element', 'layer', 'jquery'], function () {
 	// var datatime = '2019-03-02'
 	// console.log(datatime)
 
-	var url = path + "/user/group/findAllGroup.do";
-	console.log("请求controller的url是:" + url)
-	$.ajax({
-		url: url,
-		type: "get",
-		dataType: "json",
-		success: function (data) {
-			if (data.status == 0) {
-				console.log(data)
-				$("#test1").html(JSON.stringify(data));
-			}
-		},
-		error: function () {
-			console.log("ajax请求失败");
-		}
-	});
+	// var url = path + "/user/group/findAllGroup.do";
+	// console.log("请求controller的url是:" + url)
+	// $.ajax({
+	// 	url: url,
+	// 	type: "get",
+	// 	dataType: "json",
+	// 	success: function (data) {
+	// 		if (data.status == 0) {
+	// 			console.log(data)
+	// 			$("#test1").html(JSON.stringify(data));
+	// 		}
+	// 	},
+	// 	error: function () {
+	// 		console.log("ajax请求失败");
+	// 	}
+	// });
 
 	// var url = path + '/user/getPerson.do';
 	// var userId = 'user01';
@@ -47,32 +47,41 @@ layui.use(['element', 'layer', 'jquery'], function () {
 	// 	}
 	// });
 
-	//获取房间
-	// var mSize = '20';
-	// var startDate = '2019-03-05'
-	// var startTime = '17:00'
-	// var endTime = '17:30'
-	// var url = path + "/meeting/recommendRoom.do";
-	// console.log("请求controller的url是:" + url)
-	// $.ajax({
-	// 	url: url,
-	// 	type: "post",
-	// 	data: {
-	// 		'mSize': mSize,
-	// 		'startDate': startDate,
-	// 		'startTime': startTime,
-	// 		'endTime': endTime, 
-	// 	},
-	// 	dataType: "json",
-	// 	success: function (data) {
-	// 		console.log("传过来的是：")
-	// 		console.log(data)
-	// 		console.log("data.data是：" + JSON.stringify(data.data))
-	// 		$("#test1").html(JSON.stringify(data));
-	// 	},
-	// 	error: function (XMLHttpRequest, textStatus, errorThrown) {
-	// 		console.log("ajax请求失败");
-	// 	}
-	// });
+	// 提交会议
+	var mSize = '20';
+	var startDate = '2019-03-09'
+	var startTime = '17:00'
+	var endTime = '17:30'
+	var mTitle = '吃饭'
+	var mInfo = '无'
+	var 
+	var url = path + "/meeting/recommendRoom.do";
+	console.log("请求controller的url是:" + url)
+	$.ajax({
+		url: url,
+		type: "post",
+		data: {
+			'mTitle': mTitle,
+			'mInfo': mInfo,
+			'mSize': mSize,
+			'mSpan': mSpan,
+			'pId_FQ': pId_FQ,
+			'rId': rId,
+			'startDate': startDate,
+			'startTime': startTime,
+			'endTime': endTime,
+			'attendList': attendList,
+		},
+		dataType: "json",
+		success: function (data) {
+			console.log("传过来的是：")
+			console.log(data)
+			console.log("data.data是：" + JSON.stringify(data.data))
+			$("#test1").html(JSON.stringify(data));
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			console.log("ajax请求失败");
+		}
+	});
 
 });
