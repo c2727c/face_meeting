9c488d92835a5fcbe317dc7018ce126d1daf6d21
JiawangLiu <2319277867@qq.com>
2019-03-12 22:27:30 +0800

    “邮件单发以及群发

--- face_meeting/src/main/java/cn/drrs/face_meeting/controller/user/SendMailController.java
+++ face_meeting/src/main/java/cn/drrs/face_meeting/controller/user/SendMailController.java
@@ -2,6 +2,8 @@ package cn.drrs.face_meeting.controller.user;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.MessagingException;
@@ -13,6 +15,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cn.drrs.face_meeting.util.MailUtil;
 
 
 /**
@@ -26,6 +30,17 @@ public class SendMailController {
 	@Autowired
     private JavaMailSender javaMailSender;//在spring中配置的邮件发送的bean
  
+	@Autowired
+	private MailUtil mailUtil;
+	@RequestMapping("groupSend.do")
+	public Object groupSend() {
+		System.out.println("groupSend.do");
+		List<String> qqList = new ArrayList<String>();
+		qqList.add("2319277867@qq.com");
+		qqList.add("1659010991@qq.com");
+		mailUtil.send(qqList, "Spring的邮件发送类，剩下的步骤都是在这个类实现的。代码中的SimpleMail是封装了邮件主题和内容的一个POJO。觉得在一个方法参数中既包含主题又包含内容，不太合适，故重载了此方法。还有就是因为大多数邮箱的SMTP服务器地址都是可以通过邮箱地址算出来，简单起见，提供了一个不需要SMTP服务器地址的构造器。发送", "<p>这是使用邮件发送类，剩下的步骤都是在这个类实现的。代码中的SimpleMail是封装了邮件主题和内容的一个POJO。觉得在一个方法参数中既包含主题又包含内容，不太合适，故重载了此方法。还有就是因为大多数邮箱的SMTP服务器地址都是可以通过邮箱地址算出来，简单起见，提供了一个不需要SMTP服务器地址的构造器。的邮件功能发送的一封邮件</p>");
+		return "发送成功";
+	}
     @RequestMapping("send.do")
     public Object sendMail03(){
         MimeMessage mMessage=javaMailSender.createMimeMessage();//创建邮件对象
@@ -43,7 +58,7 @@ public class SendMailController {
             mMessageHelper.setText("<p>这是使用spring的邮件功能发送的一封邮件</p><br/>" +
                     "<a href='https://www.baidu.com>打开我的博客主页</a><br/>" +
                     "<img src='cid:fengye'>",true);//邮件的文本内容，true表示文本以html格式打开
-            File file=new File("D:/Code/枫叶.jpg");//在邮件中添加一张图片
+            File file=new File("E:/review/1.png");//在邮件中添加一张图片
             FileSystemResource resource=new FileSystemResource(file);
             mMessageHelper.addInline("fengye", resource);//这里指定一个id,在上面引用
             mMessageHelper.addAttachment("枫叶.jpg", resource);//在邮件中添加一个附件
