ffc77f121cbba47e922626950bde379e9864dc3a
xdwkeep <xdwkeep@foxmail.com>
2019-04-13 00:00:04 +0800

    大修改

--- face_meeting/src/main/webapp/js/user/common.js
+++ face_meeting/src/main/webapp/js/user/common.js
@@ -1,4 +1,4 @@
 //var path = "http://192.168.137.1:8080/face_meeting";//张羽晨
 //var path = "http://192.168.3.86:8080/face_meeting";//许栋伟
-var path = "http://localhost:8080/face_meeting";//许栋伟
-//var path = "http://101.132.72.223:8080/face_meeting";//许栋伟阿里云
+//var path = "http://localhost:8080/face_meeting";//许栋伟
+var path = "http://101.132.72.223:8080/face_meeting";//许栋伟阿里云
